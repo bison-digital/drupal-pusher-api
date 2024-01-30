d6425a611473a1a9de702576dd3077d7201a405a
Zach Bimson <zach.bimson@gmail.com>
2024-01-30 01:19:36 +0000

    Update js

--- js/pusher_api.js
+++ js/pusher_api.js
@@ -1,8 +1,7 @@
 (function ($, Drupal, drupalSettings) {
-    Drupal.behaviors.myModuleBehavior = {
+    Drupal.behaviors.pusherApiBehavior = {
         attach: function (context, settings) {
-            // Initialize Pusher and make it globally accessible
-            window.myDrupalPusher = new Pusher(drupalSettings.pusherApi.appId, {
+            window.pusherApiPusher = new Pusher(drupalSettings.pusherApi.appId, {
                 cluster: drupalSettings.pusherApi.cluster
             });
         }
